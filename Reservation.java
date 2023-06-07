fee2060c2c6fcbd04abebe298b16d0e658c062cd
Lim Suyoung <leepari20@gmail.com>
2023-06-07 12:52:06 +0900

    CustomerInformation 추가

--- Reservation.java
+++ Reservation.java
@@ -1,10 +1,6 @@
 package hotels;
 
-import memo.Memo;
 
-import java.sql.SQLOutput;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,19 +8,20 @@ import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Reservation {
-    int textNum = 0;
-    Room room = new Room("", "", "", 0);
+    String id;
+    Hotel hotel = new Hotel();
+    CustomerInformation customerInformation = new CustomerInformation();
+
     // 호텔 기능 메서드
     public void hotel() {
         System.out.println("1. 예약확인    2. 취소");
         Scanner sc = new Scanner(System.in);
         String hotels = sc.nextLine();
 
-        if(hotels.equals("1")) {
-            // 모든 예약 목록 조회 기능
-            System.out.println("sdsd");
+        if (hotels.equals("1")) {
+            hotel.printReservationInformation();
 
-        } else if(hotels.equals("2")) {
+        } else if (hotels.equals("2")) {
             // 메인으로 돌아가기
             System.out.println("메인으로 돌아갑니다.");
         }
@@ -37,14 +34,28 @@ public class Reservation {
         Scanner sc = new Scanner(System.in);
         String customers = sc.nextLine();
 
-        if(customers.equals("1")) {
+        if (customers.equals("1")) {
             // 예약 메서드
             reservation();
-        } else if(customers.equals("2")) {
-            // 고객 자신의 예약목록 조회 메서드
-            printReservationInformation();
-        } else if(customers.equals("3")) {
-            // 예약취소 메서드
+        } else if (customers.equals("2")) {
+            System.out.println("예약번호를 입력하세요.");
+            try {
+                String id = sc.nextLine().trim();
+                customerInformation.searchReservation(id);
+            } catch (HotelsBadException h) {
+                h.getMessage();
+            }
+
+
+        } else if (customers.equals("3")) {
+            System.out.println("예약번호를 입력하세요.");
+            try {
+                String id = sc.nextLine().trim();
+                customerInformation.cancelReservation(id);
+                System.out.println("예약이 취소되었습니다.");
+            } catch (HotelsBadException h) {
+                h.getMessage();
+            }
         }
     }
 
@@ -72,15 +83,13 @@ public class Reservation {
                 phoneNum = sc.nextLine().trim();
 
                 String pattern = "^\\d{2,3}-\\d{3,4}-\\d{4}$";
-              
+
                 if (!(Pattern.matches(pattern, phoneNum))) {
                     System.out.println("올바른 휴대전화 형식이 아닙니다. ");
                 } else {
                     break;
                 }
             }
-
-
 
             System.out.print("\n소지금을 입력해주세요. : ");
             String money = sc.nextLine().trim();
@@ -91,27 +100,10 @@ public class Reservation {
             System.out.println("예약이 완료되었습니다.");
             System.out.println();
 
-            inputReservationInformation(textNum, room, customerName, phoneNum, appointmentDate);
-            textNum++;
+            hotel.inputReservationInformation(id, room, customerName, phoneNum, appointmentDate);
 
         } else if (confirm == 2) {
             System.out.println("메인으로 돌아갑니다");
         }
     }
-
-    Map<Integer, ReservationInformation> hotelList = new LinkedHashMap<>();
-    ReservationInformation reservationinformation = new ReservationInformation("", "", "", "");
-
-    public void inputReservationInformation(int textNum, String room, String customerName, String phoneNumber, String appointmentDate) {
-        ReservationInformation reservationinformation = new ReservationInformation(room, customerName, phoneNumber, appointmentDate);
-        hotelList.put(textNum,reservationinformation);
-    }
-
-    public void printReservationInformation() {
-        Set<Integer> keySet = hotelList.keySet();
-        for (Integer key : keySet) {
-            System.out.println(key+1 + ". \n" + hotelList.get(key));
-        }
-    }
-
 }
